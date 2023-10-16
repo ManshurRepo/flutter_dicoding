408763ed86e3c765967b516a8495304dba9bcb36
ManshurRepo <muhammadtm35@gmail.com>
2023-10-16 21:11:22 +0700

    materi expand & flexible wdget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 /*void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -144,7 +142,8 @@ class _BiggerTeksState extends State<BiggerTeks> {
   }
 }*/
 
-import 'package:flutter/material.dart';
+// dropdown Button
+/*import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
@@ -162,8 +161,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  String? language;
 
   @override
   Widget build(BuildContext context) {
@@ -187,17 +193,614 @@ class FirstScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: Column(
-        children: const <Widget>[
-          Text(
-            'Sebuah Judul',
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      body: DropdownButton<String>(
+        items: const <DropdownMenuItem<String>>[
+          DropdownMenuItem<String>(
+            value: 'Dart',
+            child: Text('Dart'),
           ),
-          Text('Lorem ipsum dolor sit amet'),
+          DropdownMenuItem<String>(
+            value: 'Kotlin',
+            child: Text('Kotlin'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Swift',
+            child: Text('Swift'),
+          ),
         ],
+        value: language,
+        hint: const Text('Select Language'),
+        onChanged: (String? value) {
+          setState(() {
+            language = value;
+          });
+        },
       ),
       floatingActionButton:
           FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
+    );
+  }
+}*/
+
+// Input Widget
+/*import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  String? _name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Write your name here...',
+                  labelText: 'Your name',
+                ),
+                onChanged: (String value) {
+                  setState(() {
+                    _name = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                child: const Text('Submit'),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Text('Hello, $_name'),
+                        );
+                      });
+                },
+              )
+            ],
+          )),
+    );
+  }
+} */
+
+// input widget controller text
+/*void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your name',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text('Hello, ${_controller.text}'),
+                      );
+                    });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+} */
+
+//Switch
+
+/* import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  bool lightOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Switch(
+        value: lightOn,
+        onChanged: (bool value) {
+          setState(() {
+            lightOn = value;
+          });
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(lightOn ? 'Light On' : 'Light Off'),
+              duration: Duration(seconds: 1),
+            ),
+          );
+        },
+      ),
+    );
+  }
+} */
+
+/*
+//Radio
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  String? language;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: Radio<String>(
+              value: 'Dart',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Dart'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+                value: 'Kotlin',
+                groupValue: language,
+                onChanged: (String? value) {
+                  setState(() {
+                    language = value;
+                    showSnackbar();
+                  });
+                }),
+            title: Text('Kotlin'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Swift',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Swift'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showSnackbar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$language selected'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+} 
+*/
+//CheckBox
+
+/*import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen());
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  bool agree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Custom Font',
+          style: TextStyle(
+            fontFamily: 'Oswald',
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+} */
+
+// List View
+
+//import 'package:flutter/material.dart';
+
+/*void main() {
+  runApp(ScrollingScreen());
+}
+
+class ScrollingScreen extends StatelessWidget {
+  const ScrollingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Center(
+                child: Text(
+                  '1',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            ),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Center(
+                child: Text(
+                  '2',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            ),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Center(
+                child: Text(
+                  '3',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            ),
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Center(
+                child: Text(
+                  '4',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
+/*void main() {
+  runApp(ScrollingScreen());
+}
+
+class ScrollingScreen extends StatelessWidget {
+  const ScrollingScreen({Key? key}) : super(key: key);
+
+  final List<int> numberList = const <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: numberList.map((number) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                border: Border.all(color: Colors.white),
+              ),
+              child: Center(
+                child: Text(
+                  '$number', // Ditampilkan sesuai item
+                  style: const TextStyle(
+                      fontSize: 50, fontFamily: 'Oswald', color: Colors.white),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+*/
+
+// listView builder
+
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ScrollingScreen());
+}
+
+class ScrollingScreen extends StatelessWidget {
+  ScrollingScreen({Key? key}) : super(key: key);
+
+  final List<int> urutanNo = [
+    1,
+    2,
+    3,
+    4,
+    5,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: urutanNo.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: Center(
+                child: Text(
+                  '${urutanNo[index]}',
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}*/
+
+//listView separated
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ScrollingScreen());
+}
+
+class ScrollingScreen extends StatelessWidget {
+  ScrollingScreen({Key? key}) : super(key: key);
+
+  final List<int> urutanNomor = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.separated(
+          itemCount: urutanNomor.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                border: Border.all(color: Colors.black),
+              ),
+              child: Center(
+                child: Text(
+                  '${urutanNomor[index]}',
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
+        ),
+      ),
+    );
+  }
+} */
+
+//Expanded & flexible
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Rainbow());
+}
+
+class Rainbow extends StatelessWidget {
+  const Rainbow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 }
